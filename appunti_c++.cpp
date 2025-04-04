c01cfe9bff3fd474118517136b00d7d92d2b88e9
Lucia Giuffrida <lucia.giuffrida@mail.polimi.it>
2025-04-04 21:50:57 +0200

    appunti c++

---
+++ appunti_c++.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string nome = "lucia"; // In C++ usiamo std::string invece di char array
+    bool online= true; 
+    float temp = 0;
+    float media[5] = {0}; // Array di float con 5 elementi inizializzati a 0
+
+    std::cout << "Nome: " << temp << std::endl;
+
+    //std::cout << "ciao sono Lucia";
+    return 0;
+
+   
+}
+
